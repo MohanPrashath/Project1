15f46dc65d8afcb169a56f85037798f2e0806195
MohanPrashath <144798789+MohanPrashath@users.noreply.github.com>
2024-04-21 00:15:41 +0530

    Delete project1 directory

--- project1/TeamDDMods/app/src/main/java/com/mgsofttech/ddmods/BuildConfig.java
+++
@@ -1,6 +0,0 @@
-package com.mgsofttech.ddmods;
-
-public class BuildConfig {
-
-    public static final String VERSION_NAME = "1.0.0";
-}
--- project1/TeamDDMods/app/src/main/java/com/mgsofttech/ddmods/InterfaceProduct.java
+++
@@ -1,9 +0,0 @@
-package com.mgsofttech.ddmods;
-
-import com.mgsofttech.ddmods.model.ModelServer;
-
-public interface InterfaceProduct {
-    void onFailed(String str);
-
-    void onSuccess(ModelServer modelServer);
-}
